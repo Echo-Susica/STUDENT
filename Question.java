5b194807454751ff13905920eacc74496a3861c9
Echo_Susica~ <75769761+Echo-Susica@users.noreply.github.com>
2021-04-05 08:24:08 +0800

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,40 @@
+
+
+public class Question {
+    private int num1;
+    private int num2;
+    private int res;
+    private String sign;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getRes() {
+        return res;
+    }
+
+    public void setRes(int res) {
+        this.res = res;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
